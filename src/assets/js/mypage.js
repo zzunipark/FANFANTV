3fea1dde0b0de934c182ed587ff315f56f3ad355
MinJun Park <gusoong0829@naver.com>
2023-09-22 09:45:51 +0900

    commit.b002

--- src/assets/js/mypage.js
+++ src/assets/js/mypage.js
@@ -20,6 +20,9 @@ let currentPasswordInput = document.getElementById("currentPassword");
 let newPasswordInput = document.getElementById("newPassword");
 const changePasswordBtn = document.getElementById("changepwButton");
 
+const newPasswordError = document.getElementById("newPasswordError");
+const currentPasswordError = document.getElementById("currentPasswordError");
+
 onAuthStateChanged(auth, (user) => {
   if (user) {
     console.log("Logged in.");
@@ -29,16 +32,22 @@ onAuthStateChanged(auth, (user) => {
       const currentPassword = currentPasswordInput.value;
       const newPassword = newPasswordInput.value;
 
+      currentPasswordError.innerHTML = "";
+      newPasswordError.innerHTML = "";
+
       signInWithEmailAndPassword(auth, user.email, currentPassword)
         .then((userCredential) => {
           const user = userCredential.user;
           updatePassword(user, newPassword)
             .then(() => {
               console.log("암호를 성공적으로 변경하였습니다.");
-              firebaselogout();
+              currentPasswordError.innerHTML = "";
             })
             .catch((error) => {
               console.error("비밀번호 변경 실패:", error);
+              if (error.code === "auth/wrong-password") {
+                currentPasswordError.innerHTML = "현재 비밀번호와 일치하지 않습니다.";
+              }
             });
         })
         .catch((error) => {
