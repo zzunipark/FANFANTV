dc4f3444dfc473fd8606fc35a043095b845fc296
MinJun Park <gusoong0829@naver.com>
2024-07-11 09:54:52 +0900

    Updated Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import MainPage from "./components/pages/MainPage";
-import LoginPage from "./components/pages/LoginPage";
-import SignupPage from "./components/pages/SignupPage";
-import ForgotPasswordPage from "./components/pages/ForgotPasswordPage";
 import GlobalStyle from "./components/public/GlobalStyle";
-import NotfoundPage from "./components/pages/NotfoundPage";
-import TermsOfServicePage from "./components/pages/TermsOfServicePage";
-import PrivacyPolicyPage from "./components/pages/PrivacyPolicyPage";
-import RefuseCollectEmail from "./components/pages/RefuseCollectEmail";
-import ServiceStatus from "./components/pages/ServiceStatus";
+import { MainPage, LoginPage, SignupPage, ForgotPasswordPage, NotFoundPage, TermsOfServicePage, PrivacyPolicyPage, RefuseCollectEmailPage, ServiceStatusPage } from "./components/pages/index";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -19,11 +11,11 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/*" element={<NotfoundPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
         <Route path="/terms-of-service" element={<TermsOfServicePage />} />
         <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-        <Route path="/refuse-collect-email" element={<RefuseCollectEmail />} />
-        <Route path="/service-status" element={<ServiceStatus />} />
+        <Route path="/refuse-collect-email" element={<RefuseCollectEmailPage />} />
+        <Route path="/service-status" element={<ServiceStatusPage />} />
       </Routes>
     </Router>
   );
