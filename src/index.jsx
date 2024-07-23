c038146be5438820002beaf6ccb61efc6a03b173
MinJun Park <gusoong0829@naver.com>
2024-07-23 14:28:55 +0900

    ♻️ :: Refactor Code

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import GlobalStyle from "./components/public/GlobalStyle";
+import { MainPage, LoginPage, SignupPage, ForgotPasswordPage, NotFoundPage, TermsOfServicePage, PrivacyPolicyPage, RefuseCollectEmailPage, ServiceStatusPage } from "./components/pages/index";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+        <Route path="/terms-of-service" element={<TermsOfServicePage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+        <Route path="/refuse-collect-email" element={<RefuseCollectEmailPage />} />
+        <Route path="/service-status" element={<ServiceStatusPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
